b0a97b5830fda067954233b7b63ca49e9e3033c0
Shu Liu <liushu_public@yeah.net>
2023-12-09 13:49:56 +0800

    correct data, but bug exits for N[3] = 0

--- data/prob1.py
+++ data/prob1.py
@@ -3,7 +3,7 @@ import numpy as np
 
 chemicals = ['Acid', 'Caustic']
 tanker_type = ['A', 'B', 'C', 'D']
-demand_point = [1, 2, 3, 4, 5, 6, 7, '8_a', '8_b', 'b_c', 9, 10, 11, 12, 13]
+demand_point = [1, 2, 3, 4, 5, 6, 7, '8_a', '8_b', '8_c', 9]
 route = [
     (9, 5, 9),
     (9, 1, 9),
@@ -28,20 +28,30 @@ route = [
 N = [1, 5, 6, 0]
 
 # length of routes(km): L[r]
-L = [260, 450, 480, 340, 355, 370, 515, 550, 840, 770, 820, 435, 305]
+L = [260, 450, 480, 340, 355, 370, 515, 550, 840, 770, 820, 435, 305, 340, 355, 340, 355]
 
 # driving hours for each route: H[r]
 H = [11, 13, 15, 13, 12, 12, 23, 24, 30, 26, 30, 13, 11, 13, 12, 13, 12]
 
 # If route r contains demand point a: I[r][a]
-I = [
+# I = [[1 if a in r else 0 for a in demand_point] for r in route]  # shape: (17, 11)
 
-]  # shape: (17, 11)
+
+def get_J(_route: list[tuple], _demand_point: list) -> np.array:
+    J = np.zeros(shape=(len(_route), len(_demand_point), len(_demand_point)))
+    for r_i in range(len(_route)):
+        r = _route[r_i]
+        for i in range(len(r) - 1):
+            current_demand_point = r[i]
+            next_demand_point = r[i + 1]
+            current_demand_point_id = _demand_point.index(r[i])
+            next_demand_point_id = _demand_point.index(r[i + 1])
+            J[r_i][current_demand_point_id][next_demand_point_id] = 1
+    return J
+
 
 # In route r, if demand point a is in front of demand point b: J[r][a][b]
-J = [
-
-]  # shape: (17, 11, 11)
+J = get_J(route, demand_point)  # shape: (17, 11, 11)
 
 # Weight limit for each type of chemical at each demand point: W[a][m]
 W = [
@@ -56,20 +66,22 @@ W = [
     [6, float('inf')],  # 8_b
     [15, float('inf')],
     [15, float('inf')],
-    [15, float('inf')],
 ]  # shape: (11, 2)
 
 # Capacity for each tanker[t, k] for each chemical: C[t][k][m]
 C = [
-    [],
-    [],
-    [],
-    [],
+    [(16, 0) for j in range(N[0])],
+    [(0, 16) for j in range(N[1])],
+    [(6, 16) for j in range(N[2])],
+    [(16, 6) for j in range(N[3])],
 ]  # shape: (4, N[t], 2)
 
 # num of virtual tankers for tanker[t, k]
 V = [
-
+    [200] * N[0],
+    [200] * N[1],
+    [200] * N[2],
+    [200] * N[3],
 ]  # shape: (4, N[t])
 
 # demand for each chemical of each demand point: D[a][m]
@@ -91,9 +103,10 @@ D = [
 
 assert len(chemicals) == 2, len(chemicals)
 assert len(tanker_type) == len(N) == 4, (len(tanker_type), len(N))
-assert len(route) == len(H) == len(L) == 17, (len(demand_point), len(H), len(route), len(L))
-assert np.array(I).shape == (17, 11), np.array(I).shape
-assert np.array(J).shape == (17, 11, 11), np.array(J).shape
+assert len(demand_point) == 11, len(demand_point)
+assert len(route) == len(H) == len(L) == 17, (len(route), len(H), len(L))
+# assert np.array(I).shape == (17, 11), np.array(I).shape
+assert J.shape == (17, 11, 11), J.shape
 assert np.array(W).shape == (11, 2), np.array(W).shape
 assert len(C) == 4, len(C)
 assert len(V) == 4, len(V)
