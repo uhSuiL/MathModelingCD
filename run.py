acd7ec9b4bbba98b09a9e50e9917a46fedcbf6ac
Shu Liu <liushu_public@yeah.net>
2023-12-10 16:58:51 +0800

    fix bugs for gurobi prob1

--- run.py
+++ run.py
@@ -1,19 +1,44 @@
 from data.prob1 import *
 from model import pulp_prob1
+from datetime import datetime
+from model import gurobi_prob1
+import sys
 
+sys.setrecursionlimit(10000)
+
+tanker_types = list(range(len(tanker_types)))
+route = list(range(len(route)))
+demand_point = list(range(len(demand_point)))
+chemicals = list(range(len(chemicals)))
 
 if __name__ == '__main__':
-    prob1_model = pulp_prob1.solve(
-        list(range(len(tanker_types))),
-        list(range(len(route))),
-        list(range(len(demand_point))),
-        list(range(len(chemicals))),
-        N,
-        L,
-        H,
-        J,
-        W,
-        C,
-        V,
-        D,
-    )
+	# prob1_model = pulp_prob1.solve(
+	#     tanker_types,
+	#     route,
+	#     demand_point,
+	#     chemicals,
+	#     N,
+	#     L,
+	#     H,
+	#     J,
+	#     W,
+	#     C,
+	#     V,
+	#     D,
+	# )
+
+	model, x, y = gurobi_prob1.solve(
+		tanker_types,
+		route,
+		demand_point,
+		chemicals,
+		N,
+		L,
+		H,
+		J,
+		W,
+		C,
+		V,
+		D,
+		f'v_tk=700<{datetime.now().strftime("%Y-%m-%d(%H:%M:%S)")}>'
+	)
