810d3336c37dca090bd59f2b152d0a7f701e614f
Shu Liu <liushu_public@yeah.net>
2023-12-11 19:29:34 +0800

    beautify but infeasible

--- model/gurobi_prob1.py
+++ model/gurobi_prob1.py
@@ -19,216 +19,130 @@ def solve(
         D,
         info
 ):
+    if not os.path.exists(f'./output/prob1/{info}/'):
+        os.makedirs(f'./output/prob1/{info}/')
+
     model = gp.Model()
-    if not os.path.exists(f'./output/{info}/'):
-        os.mkdir(f'./output/{info}/')
+    model.setParam(gp.GRB.Param.LogFile, f'./output/prob1/{info}/log.log')
+
     # decision variables
-    y = [
-        [
-            model.addVar(vtype=gp.GRB.BINARY, name=f'y[{t}][{k}]')
-            for k in range(N[t])
-        ]
-        for t in tanker_types
-    ]  # shape: (len(tanker_types), len(range(N[t]))) -- y[t][k]
-    # y = model.addVars([(t, k) for t in tanker_types for k in range(N[t])], vtype=gp.GRB.BINARY, name="y")
+    # shape: (len(tanker_types), len(range(N[t]))) -- y[t][k]
+    y = model.addVars([(t, k) for t in tanker_types for k in range(N[t])], vtype=gp.GRB.BINARY) # , name="y")
+    print("y added")
 
-    z = [
-        [
-            [
-                [
-                    model.addVar(vtype=gp.GRB.BINARY, name=f'z[{t}][{k}][{n}][{r}]')
-                    for r in route
-                ]
-                for n in range(V[t][k])
-            ]
-            for k in range(N[t])
-        ]
+    z = model.addVars([
+        (t, k, n, r)
         for t in tanker_types
-    ]
-    # z = model.addVars([
-    #     (t, k, n, r)
-    #     for t in tanker_types
-    #     for k in range(N[t])
-    #     for n in range(V[t][k])
-    #     for r in route
-    # ], vtype=gp.GRB.BINARY, name='z')
+        for k in range(N[t])
+        for n in range(V[t][k])
+        for r in route
+    ], vtype=gp.GRB.BINARY) # , name='z')
+    print("z added")
 
-    x = [
-        [
-            [
-                [
-                    [
-                        [
-                            [
-                                model.addVar(vtype=gp.GRB.INTEGER, lb=0, name=f'x[{t}][{k}][{n}][{r}][{a}][{b}][{m}]')
-                                for m in chemicals
-                            ]
-                            for b in demand_point
-                        ]
-                        for a in demand_point
-                    ]
-                    for r in route
-                ]
-                for n in range(V[t][k])
-            ]
-            for k in range(N[t])
-        ]
+    x = model.addVars([
+        (t, k, n, r, a, b, m)
         for t in tanker_types
-    ]
-    # x = model.addVars([
-    #     (t, k, n, r, a, b, m)
-    #     for t in tanker_types
-    #     for k in range(N[t])
-    #     for n in range(V[t][k])
-    #     for r in route
-    #     for a in demand_point
-    #     for b in demand_point
-    #     for m in chemicals
-    # ], vtype=gp.GRB.INTEGER, lb=0, name='x')
+        for k in range(N[t])
+        for n in range(V[t][k])
+        for r in route
+        for a in demand_point
+        for b in demand_point
+        for m in chemicals
+    ], vtype=gp.GRB.CONTINUOUS, lb=0) # , name='x')
+    print("x added")
 
     model.update()
     print("decision variables added")
 
     # objective
-    model.setObjective(
-        1000 * sum([y[t][k]
-                    for t in tanker_types for k in range(N[t])]) +
-        0.36 * sum([z[t][k][n][r] * L[r]
-                    for t in tanker_types for k in range(N[t]) for n in range(V[t][k]) for r in route])
-    )
 
-    # model.setObjective(
-    #     1000 * sum([
-    #         y[t, k]
-    #         for t in tanker_types
-    #         for k in range(N[t])
-    #     ]) +
-    #     0.36 * sum([
-    #         z[t, k, n, r] * L[r]
-    #         for t in tanker_types
-    #         for k in range(N[t])
-    #         for n in range(V[t][k])
-    #         for r in route
-    #     ])
-    # )
+    model.setObjective(
+        1000 * sum([
+            y[t, k]
+            for t in tanker_types
+            for k in range(N[t])
+        ]) +
+        0.36 * sum([
+            z[t, k, n, r] * L[r]
+            for t in tanker_types
+            for k in range(N[t])
+            for n in range(V[t][k])
+            for r in route
+        ])
+    )
     print("objective added")
 
     # constraints
-    for t in tanker_types:
-        for k in range(N[t]):
-            for n in range(V[t][k]):
-                model.addConstr(sum([z[t][k][n][r] for r in route]) <= 1)
 
-    # model.addConstrs([
-    #     sum([z[t, k, n, r] for r in route]) <= 1
-    #     for t in tanker_types
-    #     for k in range(N[t])
-    #     for n in range(V[t][k])
-    # ], name="C1")
+    model.addConstrs((
+        sum([z[t, k, n, r] for r in route]) <= 1
+        for t in tanker_types
+        for k in range(N[t])
+        for n in range(V[t][k])
+    ), name="C1")
     print("constraint1 added")
 
-    for t in tanker_types:
-        for k in range(N[t]):
-            model.addConstr(sum([z[t][k][n][r] for r in route for n in range(V[t][k])]) <= y[t][k] * V[t][k])
-
-    # model.addConstrs([
-    #     sum([z[t, k, n, r] for r in route for n in range(V[t][k])]) <= y[t, k] * V[t][k]
-    #     for t in tanker_types
-    #     for k in range(N[t])
-    # ], name="C2")
+    model.addConstrs((
+        sum([z[t, k, n, r] for r in route for n in range(V[t][k])]) <= y[t, k] * V[t][k]
+        for t in tanker_types
+        for k in range(N[t])
+    ), name="C2")
     print("constraint2 added")
 
-    for t in tanker_types:
-        for k in range(N[t]):
-            for r in route:
-                for n in range(V[t][k]):
-                    for a in demand_point:
-                        for b in demand_point:
-                            for m in chemicals:
-                                model.addConstr(
-                                    x[t][k][n][r][a][b][m] <= C[t][k][m] * z[t][k][n][r] * J[r][a][b],
-                                )
-
-    # model.addConstrs([
-    #     x[t, k, n, r, a, b, m] <= C[t][k][m] * z[t, k, n, r] * J[r][a][b]
-    #     for t in tanker_types
-    #     for k in range(N[t])
-    #     for r in route
-    #     for n in range(V[t][k])
-    #     for a in demand_point
-    #     for b in demand_point
-    #     for m in chemicals
-    # ], name="C3")
+    model.addConstrs((
+        x[t, k, n, r, a, b, m] <= C[t][k][m] * z[t, k, n, r] * J[r][a][b]
+        for t in tanker_types
+        for k in range(N[t])
+        for r in route
+        for n in range(V[t][k])
+        for a in demand_point
+        for b in demand_point
+        for m in chemicals
+    ), name="C3")
     print("constraint3 added")
 
-    for b in demand_point:
-        for m in chemicals:
-            model.addConstr(
-                sum([x[t][k][n][r][a][b][m] for t in tanker_types for k in range(N[t]) for r in route for n in range(V[t][k]) for a in demand_point])
-                - sum([x[t][k][n][r][b][c][m] for t in tanker_types for k in range(N[t]) for r in route for n in range(V[t][k]) for c in demand_point])
-                == D[b][m],
-            )
+    model.addConstrs((
+        sum([
+            x[t, k, n, r, a, b, m]
+            for t in tanker_types
+            for k in range(N[t])
+            for r in route
+            for n in range(V[t][k])
+            for a in demand_point
+        ])
+        - sum([
+            x[t, k, n, r, b, c, m]
+            for t in tanker_types
+            for k in range(N[t])
+            for r in route
+            for n in range(V[t][k])
+            for c in demand_point]
+        )
+        >= D[b][m]
 
-    # model.addConstrs([
-    #     sum([
-    #         x[t, k, n, r, a, b, m]
-    #         for t in tanker_types
-    #         for k in range(N[t])
-    #         for r in route
-    #         for n in range(V[t][k])
-    #         for a in demand_point
-    #     ])
-    #     - sum([
-    #         x[t, k, n, r, b, c, m]
-    #         for t in tanker_types
-    #         for k in range(N[t])
-    #         for r in route
-    #         for n in range(V[t][k])
-    #         for c in demand_point]
-    #     )
-    #     == D[b][m]
-    #
-    #     for b in demand_point
-    #     for m in chemicals
-    # ], name="C4")
+        for b in demand_point
+        for m in chemicals
+    ), name="C4")
     print("constraint4 added")
 
-    for t in tanker_types:
-        for k in range(N[t]):
-            for r in route:
-                for n in range(V[t][k]):
-                    for b in demand_point:
-                        for m in chemicals:
-                            model.addConstr(
-                                sum([x[t][k][n][r][a][b][m] for a in demand_point])
-                                - sum([x[t][k][n][r][b][c][m] for c in demand_point])
-                                <= W[b][m],
-                            )
-
-    # model.addConstrs([
-    #     sum([x[t, k, n, r, a, b, m] for a in demand_point])
-    #     - sum([x[t, k, n, r, b, c, m] for c in demand_point])
-    #     <= W[b][m]
-    #     for t in tanker_types
-    #     for k in range(N[t])
-    #     for r in route
-    #     for n in range(V[t][k])
-    #     for b in demand_point
-    #     for m in chemicals
-    # ], name="C5")
+    model.addConstrs((
+        sum([x[t, k, n, r, a, b, m] for a in demand_point])
+        - sum([x[t, k, n, r, b, c, m] for c in demand_point])
+        <= W[b][m]
+        for t in tanker_types
+        for k in range(N[t])
+        for r in route
+        for n in range(V[t][k])
+        for b in demand_point
+        for m in chemicals
+    ), name="C5")
     print("Constraint 5 added")
 
-    for t in tanker_types:
-        for k in range(N[t]):
-            model.addConstr(
-                sum([z[t][k][n][r] * H[r] for r in route for n in range(V[t][k])]) <= 5240,
-            )
-
-    # model.addConstrs([
-    #     sum([z[t, k, n, r] * H[r] for r in route for n in range(V[t][k])]) <= 5240
-    #     for t in tanker_types
-    #     for k in range(N[t])
-    # ], name="C6")
+    model.addConstrs((
+        sum([z[t, k, n, r] * H[r] for r in route for n in range(V[t][k])]) <= 5240
+        for t in tanker_types
+        for k in range(N[t])
+    ), name="C6")
     print("Constraint 6 added")
 
     print("constraints added")
@@ -237,9 +151,28 @@ def solve(
     model.optimize()
     print("finish solving")
 
-
-    model.write(f'./output/{info}/gurobi_model[{info}].mps')
-    model.write(f'./output/{info}/gurobi_model[{info}].json')
-    model.write(f'./output/{info}/gurobi_model[{info}].ilp')
-    print("files written")
+    write_model(f'./output/prob1/{info}/gurobi_model[{info}]', model)
+    display_model(model)
     return model, x, y, z
+
+
+def write_model(path: str, model):
+    suffix_list = ['mps', 'json', 'attr', 'sol', 'hnt']
+    for suffix in suffix_list:
+        try:
+            model.write(path + '.' + suffix)
+            print(suffix + ' written')
+        except Exception as e:
+            print(e)
+    try:
+        if model.status == 3:  # infeasible
+            model.computeIIS()
+            model.write(path + '.ilp')
+    except Exception as e:
+        print(e)
+    print("files written")
+
+
+def display_model(model):
+    model.printStats()
+    # model.display()
